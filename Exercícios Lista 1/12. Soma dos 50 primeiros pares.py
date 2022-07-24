b5a3927308eeb7893b3932c9419807103a65f089
NickBR <109099149+NicolausBR@users.noreply.github.com>
2022-07-24 10:42:36 -0300

    Update 12. Soma dos 50 primeiros pares.py

--- Exercícios Lista 1/12. Soma dos 50 primeiros pares.py
+++ Exercícios Lista 1/12. Soma dos 50 primeiros pares.py
@@ -1,6 +1,6 @@
 soma = 0
 num = int(50)
-for pares in range(2, num+num, 2):
+for pares in range(2, num+num+1, 2):
     soma += pares
 
 print(f'A soma dos 50 primeiros números pares é igual a = {soma}')
